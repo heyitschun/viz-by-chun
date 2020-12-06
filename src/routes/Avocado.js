458c50bcf9ad300cc42b746cf623c25e2f809392
heyitschun <cpoon19@gmail.com>
2020-12-06 13:28:51 +0100

    avp line chart

--- src/routes/Avocado.js
+++ src/routes/Avocado.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Footer from '../components/footer';
-import data from '../data/avocado/organic-month-avp-vol.json';
-import { line, scaleLinear, scaleTime, timeYear } from 'd3';
+import organicData from '../data/avocado/organic-month-avp-vol.json';
+import conventionalData from '../data/avocado/conventional-month-avp-vol.json';
+import { extent, line, max, scaleLinear, scaleTime, timeYear } from 'd3';
 import { axisBottom } from 'd3-axis';
 import { select } from 'd3-selection';
 
@@ -9,28 +10,40 @@ function Avocado() {
   const dims = {
     width: 400,
     height: 400
-  }
-  let avp = [];
-  let vol = [];
-  const values = Object.values(data).map(v => {
-    avp = [...avp, v["AveragePrice"]];
-    vol = [...vol, v["Total Volume"]];
-    return v;
-  });
-  console.log(avp);
+  };
 
-  let avpScale = scaleLinear()
-    .range()
+  const timeDomain = extent(organicData, d => d.date);
+  const xScale = scaleTime()
+    .domain([new Date("2015"), new Date("2019")])
+    .range([0, dims.width])
 
-  const lineGenerator = line();
+  const xAxis = axisBottom().scale(xScale).ticks(timeYear);
 
-  let times = Object.keys(data);
-  let timeScale = scaleTime()
-    .domain([new Date(times[0]), new Date(times.slice(-1)[0])])
-    .range([0, 400])
+  const avpOrganicMax = max(organicData, d => d.avp);
+  const avpOrganicScale = scaleLinear()
+    .domain([0, avpOrganicMax])
+    .range([dims.height, 0]);
 
+  const avpConventionalMax = max(conventionalData, d => d.avp);
+  const avpConventionalScale = scaleLinear()
+    .domain([0, avpConventionalMax])
+    .range([dims.height, 0]);
 
-  const xAxis = axisBottom().scale(timeScale).ticks(timeYear);
+  const volMax = max(organicData, d => d.volume);
+  const volScale = scaleLinear()
+    .domain([0, volMax])
+    .range([dims.height, 0]);
+
+  const lineGenerator = line()
+  lineGenerator.x(d => xScale(new Date(d.date)));
+
+  // Organic plots
+  lineGenerator.y(d => avpOrganicScale(d.avp));
+  const avpOrganicLine = lineGenerator(organicData);
+
+  // Conventional plots
+  lineGenerator.y(d => avpConventionalScale(d.avp));
+  const avpConventionalLine = lineGenerator(conventionalData);
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -40,12 +53,13 @@ function Avocado() {
           width={dims.width}
           height={dims.height}
         >
-          <circle cy="50" cx="50" r="40" fill="#ff9966"></circle>
+          <path d={avpOrganicLine} fill="none" stroke="#ff9966" />
+          <path d={avpConventionalLine} fill="none" stroke="#dd99ff" />
           <g>
             <g
-              ref={(node) => select(node).call(xAxis)}
+              ref={node => select(node).call(xAxis)}
               className="z-50 text-black"
-              style={{ transform: `translateY(${dims.height/2}px)` }}
+              style={{ transform: `translateY(${dims.height-100}px)` }}
             />
           </g>
         </svg>
